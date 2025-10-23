31a9ae10fec07efa00b6f4246f9ed9ae62887378
Fathur Rahman <fathurrahman081296@gmail.com>
2025-10-23 09:10:46 +0800

    api eloquent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
+use App\Models\Question;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(10)
+            ->has(
+                Question::factory(5)->has(
+                    Answer::factory(15)->state(function ($attributes, Question $question) {
+                        return ['user_id' => $question->user_id];
+                    })
+                )
+            )
+            ->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,8 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    Route::resource('/questions', QuestionController::class)
+    ->except(['index', 'show'])
+    ->middleware('auth');
     return Inertia::render('Question/Index');
 });
