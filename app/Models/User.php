211623ba834f7a203a56dd023b9c65ed45d5fdaf
Fathur Rahman <fathurrahman081296@gmail.com>
2025-10-24 08:12:45 +0800

    bookmarks

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -43,12 +44,60 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => Role::class
         ];
     }
 
-    public function question()
+    public function questions()
     {
         return $this->hasMany(Question::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+
+    public function bookmarks()
+    {
+        return $this->belongsToMany(Question::class, 'bookmarks')->withTimestamps();
+    }
+
+    public function voteQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'votable');
+    }
+
+    public function voteAnswers()
+    {
+        return $this->morphedByMany(Answer::class, 'votable');
+    }
+
+    public function voteQuestion(Question $question, int $vote)
+    {
+        $voteQuestions = $this->voteQuestions();
+
+        $this->vote($voteQuestions, $question, $vote);
+    }
+
+    public function voteAnswer(Answer $answer, int $vote)
+    {
+        $voteAnswers = $this->voteAnswers();
+
+        $this->vote($voteAnswers, $answer, $vote);
+    }
+
+    public function vote($relationship, $model, $vote)
+    {
+        if ($relationship->where('votable_id', $model->id)->exists()) {
+            $relationship->updateExistingPivot($model, ['vote' => $vote]);
+        } else {
+            $relationship->attach($model, ['vote' => $vote]);
+        }
+
+        $model->load('votes');
+        $model->votes_count = $model->votes()->sum('vote');
+        $model->save();
     }
 
     public function avatarUrl()
